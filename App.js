a285f05e05ec62ed31d39eac6fb61765c2cc5ed3
Udaya-Pujari <udaysp194@gmail.com>
2024-02-02 22:01:22 +0530

    Episode 2 :Creating a own React app using parcel bundler

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
 
 const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "i am h1 tag"),
-      React.createElement("h2", {}, "I  am h2 tag"),
+      React.createElement("h1", {}, "This Namste Javascript"),
+      React.createElement("h2", {}, "by Udaya Pujari"),
     ]),
     React.createElement("div", { id: "child2" }, [
       React.createElement("h1", {}, "I am h1 tag"),
@@ -12,5 +14,5 @@ const parent = React.createElement("div", { id: "parent" }, [
   
   console.log(parent);
   
-  const root = ReactDOM.createRoot(document.getElementById("header"));
+  const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Welcome to React Journey .
+
+# What parcel Did?
+
+- It has Dev Build
+- It created a local server with port 1234
+- It did HMR- Hot Module Replacement, means It is reading all files
+- parcel uses a file watching algorithm, which is written in c++
+- breacuse of caching ,the parcel does the faster builds
+- image optimization
+- suppose if we create production build, It will minify our file
+- it will bundle all the files for us.
+- it will also compress
+- connsitant hashing
+- code splitting
+- differntial bundling : to support older browsers
+- diagnostic for our app
+- gives good error messages
+- give a way to host on https also.
+-Tree shaking algorithm : it will remove unused code for you.
+- it has diffrent build for dev and production bundles
