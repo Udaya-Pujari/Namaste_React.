620ba16f57f0e791e77e96c6fa885a1f44defa66
Udaya-Pujari <udaysp194@gmail.com>
2024-02-04 21:27:54 +0530

    Episode 2 first commit

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "This Namste Javascript"),
-      React.createElement("h2", {}, "by Udaya Pujari"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "I am h1 tag"),
-      React.createElement("h2", {}, "I am h2 tag"),
-    ]),
-  ]);
-  
-  console.log(parent);
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(parent);
+// React element i.e React.createElement => this is basically a object => when we render this element on to dom it becomes an HTML
+
+// The following line is creating react element
+const heading = React.createElement("h1", { id: "heading" }, "Namaste react");
+// console.log("react heading ==>", heading);
+
+// using JSX
+
+const jsxHeading = <h1 id="heading">Namaste react by using JSX</h1>;
+
+// console.log("jsx heading ==>", jsxHeading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
